128daa16ab17f5223812fe054ca784cef6553992
Luka Sculac <lukasculac@hotmail.com>
2024-10-23 13:08:57 +0200

    Fixed the plots and added one more type

--- main_database.py
+++ main_database.py
@@ -1,1 +1,64 @@
 
+import tkinter as tk
+from tkinter import filedialog, messagebox
+import matplotlib.pyplot as plt
+from readQ import setup_Qfile
+from readQ import nodi_plot 
+
+# Function to select a file
+def select_file():
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        file_label.config(text=file_path)  # Display file name in the label
+        global selected_file
+        selected_file = file_path  # Store the selected file path
+
+# Function to run plotting
+def run_plotting():
+    try:
+        #input_value = float(input_entry.get())  # Get user input and convert to float
+        #your_plotting_function(input_value)  # Run the plotting function with input
+        input_value = input_entry.get()  # Get user input as string
+        input_value_2 = dropdown_var.get()  # Get user input as string
+        [Time,Data,Count] = setup_Qfile(selected_file,input_value)
+        nodi_plot(Time,Data,Count,input_value_2)
+    except ValueError:
+        messagebox.showerror("Invalid input", "Please enter a valid number.")
+
+# Create the main window
+root = tk.Tk()
+root.title("NoDi* Plotter")
+
+root.geometry("600x400")
+
+# Label and button for file selection
+file_label = tk.Label(root, text="No file selected")
+file_label.pack(pady=10)
+
+select_button = tk.Button(root, text="Select File", command=select_file)
+select_button.pack(pady=5)
+
+# Input for user to enter a number
+input_label = tk.Label(root, text="Enter desired length:")
+input_label.pack(pady=5)
+
+input_entry = tk.Entry(root)
+input_entry.pack(pady=5)
+
+dropdown_label = tk.Label(root, text="Select a plot option:")
+dropdown_label.pack(pady=5)
+
+# Input for user to enter a number
+dropdown_var = tk.StringVar(root)
+dropdown_var.set("Q(i+1) and Q(i)")  # Set default option
+
+options = ["Q(i+1) and Q(i)", "T(i+1) and T(i)", "T(i) and Q(i)", "Q(i)/T(i) and T(i)"]  # List of options
+dropdown_menu = tk.OptionMenu(root, dropdown_var, *options)
+dropdown_menu.pack(pady=10)
+
+# Button to run plotting
+plot_button = tk.Button(root, text="Create Plots", command=run_plotting)
+plot_button.pack(pady=10)
+
+# Run the GUI loop
+root.mainloop()
--- readQ.py
+++ readQ.py
@@ -1,5 +1,5 @@
 
-#region Functions
+#import packages
 import numpy as np
 import matplotlib.pyplot as plt
 #from scipy.stats import gaussian_kde
@@ -9,8 +9,8 @@ from matplotlib.colors import LogNorm
 import matplotlib.ticker as ticker
 
 def setup_Qfile(sourcefile,command):
-    # number of datas to zero
-    
+
+    # the value of command determines the length of the reading  
     if not command.isdigit():
         f = open(sourcefile, 'rb')
         content = f.read()
@@ -23,7 +23,6 @@ def setup_Qfile(sourcefile,command):
     return readQ(sourcefile,count)
 
 def  readQ(sourcefile,Count):
-    #function open one or more .q files and move infromation back
     #file gets opened
     f = open(sourcefile, 'rb')
     if f == -1:
@@ -34,7 +33,7 @@ def  readQ(sourcefile,Count):
     Time = np.zeros((Count))
     # reset Matrix to Null
     Data = np.zeros((Count))
-    # read file unitl count is reached or no more infromation is avaiable
+    # read file unitl count is reached
     while (counter < Count):
         # Charge
         q = np.fromfile(f,dtype='float32',count=1) # 4 Bytes
@@ -46,88 +45,89 @@ def  readQ(sourcefile,Count):
             Time[counter]=t[0]
             Data[counter]=q[0]
             counter=counter+1
-        else:
-            # otherwise stop
-            break
     #file gets closed
     f.close()
+    # returns the three important data types
     return[Time,Data,counter]
 
 def nodi_plot(Time,Data,Count,Command):
+    #responsible for the creation of the nodi* plots
+    # sets up the matrix for the magnitude calculation
     magnitude_i = np.zeros(Count)
     magnitude_i_1 = np.zeros(Count)
-
+    # sets up the matrix for the time calculation
     time_i = np.zeros(Count)
     time_i_1 = np.zeros(Count)
-
+    # sets up the counter for the calculation; set to 2 as calculation takes PD before and after into account
     counter = 2
-
+    # performs the calculation for each PD pulse
     while(counter < (Count-1)):
+        # magnitude
         magnitude_i[counter] = Data[counter] - Data[counter-1]
         magnitude_i_1[counter] = Data[counter+1] - Data[counter]
-
+        # time
         time_i[counter] = Time[counter] - Time[counter-1]
         time_i_1[counter] = Time[counter+1] - Time[counter]
-
+        # update counter
         counter = counter + 1
 
-    # Create a scatter plot with density as color
-    #xy = np.vstack([magnitude_i, magnitude_i_1])
-    #z = gaussian_kde(xy)(xy)  # Kernel density estimation for color
+    # clean the lists
+    zero_indices_time = np.where(time_i == 0)
+    zero_indices_time_i = np.where(time_i_1 == 0)
 
-    #plt.scatter(magnitude_i,magnitude_i_1,c=z)
-    #plt.show()
+    # Remove entries from both lists at the zero indices
+    magnitude_i = np.delete(magnitude_i, zero_indices_time)
+    time_i = np.delete(time_i, zero_indices_time)
+
+    magnitude_i_1 = np.delete(magnitude_i_1, zero_indices_time_i)
+    time_i_1 = np.delete(time_i_1, zero_indices_time_i)
 
     # create 2D plot
-
-    # Create a 100x100 grid of equally spaced containers
-    grid_size = 100
-    if Command == 'Q':
+    # sets up the plot labels
+    if Command == 'Q(i+1) and Q(i)':
         x = magnitude_i
         y = magnitude_i_1
         word = 'Magnitude'
-        xlabel = 'Q\'i (C)'
-        ylabel = 'Q\'i+1 (C)'
-    elif Command == 'T':
+        xlabel = r'$\Delta q_{{i}} \, \text{(C)}$'
+        ylabel = r'$\Delta q_{{i+1}} \, \text{(C)}$'
+    elif Command == 'T(i+1) and T(i)':
         x = time_i
         y = time_i_1
         word = 'Time'
-        xlabel = 'T\'i (S)'
-        ylabel = 'T\'i+1 (S)'
-    elif Command == 'QT':
+        xlabel = r'$\Delta t_{{i}} \, \text{(s)}$'
+        ylabel = r'$\Delta t_{{i+1}} \, \text{(s)}$'
+    elif Command == 'T(i) and Q(i)':
         x = magnitude_i
         y = time_i
+        word = 'Time and Magnitude'
+        xlabel = r'$\Delta q_{{i}} \, \text{(C)}$'
+        ylabel = r'$\Delta t_{{i}} \, \text{(s)}$'
+    elif Command == 'Q(i)/T(i) and T(i)':
+        x = time_i
+        y = magnitude_i / time_i
         word = 'Magnitude and Time'
-        xlabel = 'Q\'i (C)'
-        ylabel = 'T\'i (S)'
+        xlabel = r'$\Delta t_{{i}} \, \text{(s)}$'
+        ylabel = r'$\Delta q_{{i}}/ \Delta t_{{i}} \, \text{(C/s)}$'
     else:
         msg = print('Command is wrong!')
         return 0
-
+    # Create a 100x100 grid of equally spaced containers
+    grid_size = 100
     x_bins = np.linspace(min(x), max(x), grid_size)
     y_bins = np.linspace(min(y), max(y), grid_size)
 
     # 2D histogram to count the number of points in each bin
     H, xedges, yedges = np.histogram2d(x, y, bins=[x_bins, y_bins])
 
-    # Calculate the number of minutes (assuming time is in minutes)
-    num_minutes = (max(Time) - min(Time))/60
+    # Calculate the number of minutes (assuming time is in seconds)
+    num_seconds = (max(Time) - min(Time))/60
 
     # Calculate the mean stack count per minute
-    mean_stack_count_per_min = np.sum(H) / num_minutes
+    mean_stack_count_per_min = np.sum(H) / num_seconds
 
     # Scale the stack heights by the mean stack count per minute
     H_scaled = (H / mean_stack_count_per_min)
 
-    # Apply a logarithmic scale to the stack counts for color coding
-    #for y in range(99):
-    #    for x in range(99):
-    #    
-    #        if H_scaled[x,y] != 0:
-    #            H_scaled[x,y] = np.log10(H_scaled[x,y])
-    #        else:
-    #            H_scaled[x,y] = np.nan
-    
     # Plot the log-scaled density as a heatmap
     plt.imshow(H_scaled.T, origin='lower', aspect='auto', extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]],cmap='viridis',norm=LogNorm())
     ax = plt.gca()
@@ -149,7 +149,6 @@ def nodi_plot(Time,Data,Count,Command):
     plt.show()
 
     # Create 3D plot
-
     # Create a meshgrid for the surface plot
     X, Y = np.meshgrid(xedges[:-1], yedges[:-1])
 
@@ -181,17 +180,14 @@ def nodi_plot(Time,Data,Count,Command):
     cbar.set_label('Log Scaled Density')
 
     plt.show()
-
-
-
 #endregion
 
 #command = '80000'
-command = 'end'
-filename = 'C:/Users/lukas/Downloads/Thesis/Waveforms/0001_matlab_DC/unit1.1.Q'
-[Time,Data,Count] = setup_Qfile(filename,command)
+#command = 'end'
+#filename = 'C:/Users/lukas/Downloads/Thesis/Waveforms/0001/unit1.1.Q'
+#[Time,Data,Count] = setup_Qfile(filename,command)
 
-nodi_plot(Time,Data,Count,'QT')
+#nodi_plot(Time,Data,Count,'QT')
 
 # Create the plot
 #plt.plot(Time, Data, label='PD Magnitude', marker='o')  # Line plot with markers
